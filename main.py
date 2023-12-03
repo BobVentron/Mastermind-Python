2a9b00ebc18fae5c0e2352454a2f00a5ad8d2aee
Soleilhac Bastien <bastien.soleilhac@etu.univ-grenoble-alpes.fr>
2023-12-03 12:11:14 +0100

    finition affichage MasterMind

--- main.py
+++ main.py
@@ -37,24 +37,29 @@ def gestionMastermind():
         print(CouleurATrouver)
 
         menu = """
-            Entrer des couleusr (séparer par un espace) que vous penser être la solution,
+            Entrer des couleurs (séparer par un espace) que vous penser être la solution,
             ou tapez sur "q" pour arrêter le jeu et avoir la solution.
         """
         while (choix := input(menu)) not in 'qQ' :
             listcouleur = choix.split()
+            couleurBonne = True
             for e in listcouleur : 
                 if e not in color: 
                     print("Vous avez saisi des couleurs érronner.")
+                    couleurBonne = False
             
-            if listcouleur == CouleurATrouver : 
-                print("Bravo vous avez trouver!!")
-                break
-            else :
-                print("pas trouver")
-                print(couleurCorect(CouleurATrouver, listcouleur))
-                print(couleurBienPlacer(CouleurATrouver, listcouleur))
+            if couleurBonne == True :
+                if listcouleur == CouleurATrouver : 
+                    print("Bravo vous avez trouver!!")
+                    break
+                else :
+                    bienplacer = couleurBienPlacer(CouleurATrouver, listcouleur)
+                    existe = couleurCorect(CouleurATrouver, listcouleur) - bienplacer
+                    affichage = f'Vous n\'avez pas trouver la bonne combinaison. Voici quelque indice pour vous aider a la trouver: \n Il y a {bienplacer} pion bien placé \n Il y a {existe} pion qui existe dans la combinaison mais pas à la bonne place'
+                    print(affichage)
 
-        print("La solution était : ")
+
+        print("La solution était : ", end='')
         for e in CouleurATrouver : 
             print(e + " ", end='')
 
