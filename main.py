0af341734407cacfa9c830ce0dccc3541f5920e0
Soleilhac Bastien <bastien.soleilhac@etu.univ-grenoble-alpes.fr>
2023-12-04 21:13:27 +0100

    rajout de la gestion du plus ou moins dans le main

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import Martermind.gestionCLI as gestionCliMastermind
+import plus_ou_moins.gestionCLI as GestionCliPlusOuMoins
 
 if __name__ == '__main__' :
     fini = False
     menu1 = """
         Tapez '1' pour commencer le jeu du Mastermind!
         Tapez '2' pour ...
-        Taoez 'q' pour quitter.
+        Taoez 'q' pour quitter.s
         Votre choix : 
     """
     menu2 = """
@@ -27,6 +28,13 @@ if __name__ == '__main__' :
                         case _ :
                             print("Choix non valide")
             case '2':
-                print("Choix ")
+                while (choix2 := input(menu2)) not in 'qQ' :
+                    match choix2 : 
+                        case '1':
+                            GestionCliPlusOuMoins.gestionCliPlusOuMoins()
+                        case '2' :
+                            print("La version avec interface graphique n'a pas encore été développer!")
+                        case _ :
+                            print("Choix non valide")
             case _: 
                 print("Choix non valide")
